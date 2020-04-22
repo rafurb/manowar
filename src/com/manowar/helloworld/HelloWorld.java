0c23fe1601e55e05ac813b27cc221d056670bd13
Z6RUR <RUR@kmd.dk>
2020-04-22 13:11:32 +0200

    First commit

---
+++ src/com/manowar/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.manowar.helloworld;
+
+/**
+ * Created by Z6RUR on 22.04.2020.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("Hello Worldgit ");
+    }
+}
